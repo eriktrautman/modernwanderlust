377d6283bafd4fc808f205e722ba5791e937d3fd
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-08-24 17:36:19 -0700

    posts controller

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ modernwanderlust
 ================
 
 Erik's Blog
+
+Roadmap:
+Posts
+Post creation interface
+  Have the admin bar only appear if logged in once devise is installed
+Post display
+  title stubs (unique only!)
+  title stubs in post creation option? NO, edit only.
+Mimic UI template
+  SEO titles 
+Static pages
+Add categories
+Add markdown rendering engine
+Add pagination
+Add devise user auth
+Add disqus for commenting
+
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,49 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    # build in the stub functionality
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(params[:post])
+    if post.save
+      flash[:success] = "Your post has saved successfully"
+      redirect_to post
+    else
+      flash.now[:error] = "There was a problem creating your post, error messages: #{ error_messages_for post }!"
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def update
+    post = Post.find_by_id(params[:id])
+    if post
+      if post.update_attributes(params[:post])
+        flash[:succes] = "Your post has been updated"
+        redirect_to post
+      else
+        flash.now[:error] = "There was a propblem updating your post, error messages: #{ error_messages_for post }!"
+        render :edit
+      end
+    else
+      flash.now[:error] = "Your post could not be updated because it couldn't be found!"
+      render :edit
+    end
+  end
+
+
+
 end
