a00f5c44a53af1c763bbe5ff070b2d76fea5c861
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-09-01 12:48:58 -0700

    add tags and taggings migrations and run them

---
+++ db/migrate/20130901193208_create_taggings.rb
@@ -0,0 +1,12 @@
+class CreateTaggings < ActiveRecord::Migration
+  def change
+    create_table :taggings do |t|
+      t.integer :post_id, :null => false
+      t.integer :tag_id, :null => false
+
+      t.timestamps
+    end
+
+    add_index :taggings, [:post_id, :tag_id], :unique => true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130824234053) do
+ActiveRecord::Schema.define(version: 20130901193217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,22 @@ ActiveRecord::Schema.define(version: 20130824234053) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "taggings", force: true do |t|
+    t.integer  "post_id",    null: false
+    t.integer  "tag_id",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["post_id", "tag_id"], name: "index_taggings_on_post_id_and_tag_id", unique: true, using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
 end
