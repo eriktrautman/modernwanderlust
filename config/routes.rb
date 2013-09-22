6266df87d76f39357d8bf0811803611c7cbb7af7
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-09-21 17:26:00 -0700

    add login/logout devise routes and apply protections to controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Modernwanderlust::Application.routes.draw do
 
-  devise_for :admins
+  devise_for :admins do
+  end
+
+  devise_scope :admin do
+    get "/login" => "devise/sessions#new"
+    get "/logout" => "devise/sessions#destroy"
+  end
+    
   root :to => 'posts#index'
 
   get "about" => "static_pages#about"
