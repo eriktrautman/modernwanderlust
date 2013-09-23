c4150b3c27973d3591797c3ba782dcfb74223256
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-09-22 23:04:53 -0700

    add created_at to posts form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -76,7 +76,7 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:body,:title)
+      params.require(:post).permit(:body,:title,:created_at)
     end
 
     def tag_params
