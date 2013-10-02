d546e253efff43679ce72a28478c6e4d3dc7bc60
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-10-01 20:19:55 -0700

    add md column to posts so they can be rendered selectively with markdown

---
+++ db/migrate/20131002030726_add_md_to_posts.rb
@@ -0,0 +1,13 @@
+class AddMdToPosts < ActiveRecord::Migration
+  def change
+    add_column :posts, :md, :boolean, :default => true
+
+    # set each pre-existing post to actually be false
+    # since I didn't use .md until this migration
+    Post.all.each do |post|
+      post.md = false
+      post.save!
+    end
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131002020540) do
+ActiveRecord::Schema.define(version: 20131002030726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20131002020540) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
+    t.boolean  "md",         default: true
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
