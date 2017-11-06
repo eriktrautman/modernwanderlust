1e3c7bbd73fbd10b94c9cf9f09e3871dd822cd5e
Erik Trautman <erik.trautman@gmail.com>
2017-11-05 22:45:51 -0800

    fix form display

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,7 @@ class Post < ActiveRecord::Base
               thumb: '118x100#',    # used in the file browser window
               square: '400x400#',   # used in compressed indexes
               squarelg: '800x800#',
+              reduced: '800<'
             }
   validates_attachment_content_type :header_image, content_type: /\Aimage\/.*\z/
 
